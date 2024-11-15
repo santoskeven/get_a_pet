7377c1705c4def7583847b4e4dce37d4204765eb
unknown <r.kevensantos7@gmail.com>
2024-11-15 00:46:29 -0300

    req. get pets cadast., exibir pets, css pets

--- frontend/src/components/pages/pets/mypets.js
+++ frontend/src/components/pages/pets/mypets.js
@@ -1,26 +1,72 @@
 import { Link } from "react-router-dom"
 
+import styles from './DashBoard.module.css'
+
 import { useState, useEffect } from 'react'
+import useFlashMessage from '../../../hooks/userFlashMessages'
+
+import api from '../../../utils/api'
+import RoundedImage from '../../layouts/roundedImage'
+
 
 function Mypets(){
 
-    const [pet, setPet] = useState([])
+    const [pets, setPet] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '') 
+    const {setFlashMessage} = useFlashMessage()
+
+    useEffect(() => {
+        api.get('/pets/mypets', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+        })
+        .then((res) => {
+            setPet(res.data.pets)
+        })
+    }, [token])
 
     return(
         <section>
-            <div>
+            <div className={styles.petList_header}>
                 <h1>MyPets</h1>
-                <Link to='/pet/add'>CADASTRAR PET</Link>
+                <Link to='/pets/add'>CADASTRAR PET</Link>
             </div>
 
-            {pet.length > 0 && 
-            <p>Meus pets cadastrados</p>
-            }
+            <div className={styles.petList_container}>  
 
-            {pet.length == 0 && 
-            <p>Não há pet cadastrado </p>
-            }
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                    <div className={styles.petList_row} key={pet._id}>
+                        <RoundedImage 
+                            src={`${process.env.REACT_APP_API}/image/pets/${pet.images[0]}`}
+                            alt={pet.name}
+                            width='px75'
+                        />
 
+                        <span className="bold">{pet.name}</span>
+                        <div className={styles.actions}>
+                            {pet.available ? (
+                            (<>
+                                {pet.adopter && <button className={styles.conclude_btn}> Concluir adoção</button>}
+                                <Link to={`/pet/edit/${pet._id}`}>EDITAR</Link>
+                                <button>EXCLUIR</button>
+                            </>)
+                            ): 
+                            (
+                                <p>Pet já adotado</p>
+                            )}
+                        </div>
+                    </div>
+                    //    <p>Meus pets cadastrados</p>
+                    ))
+                }
+
+                {pets.length == 0 && 
+                <p>Não há pet cadastrado </p>
+                }
+
+            </div>
         </section>
     )
 }
